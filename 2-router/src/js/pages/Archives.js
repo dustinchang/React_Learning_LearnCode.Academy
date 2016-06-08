3dcb5dea6611e96adfa5194241bf74840ce8a805
dustinchang <dustinlchang@gmail.com>
2016-06-07 22:53:40 -0700

    fin tut7, Router Params & Queries

--- 2-router/src/js/pages/Archives.js
+++ 2-router/src/js/pages/Archives.js
@@ -2,8 +2,17 @@ import React from "react";
 
 export default class Archives extends React.Component {
   render() {
+    //destructuring assignment syntax
+    const { params } = this.props; //essentially var params = this.props.params
+    const { article } = params; //var article = params.article
+    const { query } = this.props.location;
+    const { date, filter } = query;
     return (
-      <h1>Archives</h1>
+      <div>
+        <h1>Archives ({article})</h1>
+        <h4>date: {date}, filter: {filter}</h4>
+
+      </div>
     );
   }
 }
--- 2-router/src/js/pages/Layout.js
+++ 2-router/src/js/pages/Layout.js
@@ -6,11 +6,13 @@ export default class Layout extends React.Component {
     this.context.router.replace('/'); // was this.props.history.pushState(null, '/'), but props.history and pushState are being deprecated so solution to this was use contextTypes and use push, same for replaceState to use replace instead
   }
   render() {
+    const { history } = this.props;
+    console.log(history.isActive("archives"));
     return (
       <div>
         <h1>KillerNews.net!</h1>
         {this.props.children}
-        <Link to="archives" className="btn btn-danger">archives</Link>
+        <Link to="archives" activeClassName="test" className="btn btn-danger">archives</Link>
         <Link to="settings"><button className="btn btn-success">settings</button></Link>
         <button onClick={this.navigate.bind(this)}>featured</button>
       </div>
